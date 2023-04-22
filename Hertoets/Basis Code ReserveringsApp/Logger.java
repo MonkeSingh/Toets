b17d3aa635e61ed4223e26b79673370178005366
MonkeSingh <129729790+MonkeSingh@users.noreply.github.com>
2023-04-22 18:12:17 +0200

    Delete Hertoets directory

--- Hertoets/Basis Code ReserveringsApp/Logger.java
+++
@@ -1,6 +0,0 @@
-
-public class Logger {
-    //logReservering functie maken voor een standaardreservering
-
-    //tweede logReservering functie maken voor een elektrischereservering
-}
--- Hertoets/Basis Code ReserveringsApp/ParkeerplekStandaard.java
+++
@@ -1,4 +0,0 @@
-
-public class ParkeerplekStandaard extends Parkeerplaats {
-    //Maak de klasse werkend met de parent klasse
-}
